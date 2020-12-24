dbe12225fe3ffb5b523099e04e96eb104fd9c4e8
nsikak-akpakpan <CORP\nsikak@a-9itspc95z4qu.corp.amazonworkspaces.com>
2020-12-24 12:41:18 -0600

    added git comments

--- test/script.sh
+++ test/script.sh
@@ -1,1 +1,8 @@
-sample add more code to this file
+# add more code to this file
+#git commands
+## to clone repo --> git clone [git url]
+## to connect to remote repo --> git remote add [repo directory] [git url]
+## to pull git content --> git pull
+## to add file or directory --> git add [file or directory name]
+## to commit to git -->  git commit -m [commit message]
+## to push to remote -->  git push origin [branch default: master] 
